a7f3efd76bbf83ea479db3d9b1d631871de98f0f
faeze <faezeebold@gmail.com>
2023-06-05 21:01:31 +0330

    api

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import Users from './Users';
 const App = () =>{
 
     return(
-        <div> <Users/> </div>
+        <div>
+             <Users/> 
+        </div>
+        
 
     )
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,58 @@
+import { render } from "@testing-library/react";
+import axios from "axios";
+import { Component, useState } from "react";
 
 const Users = () =>{
 
+    const [user , setuser] = useState([])
+
+
+    async function ComponentDidmount(){
+
+        const response = await axios.get('https://reqres.in/api');
+        ({setuser : response })
+    }
+
+    
     return(
-        <div>Users</div>
+           <div>
+            <button className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">create</button>
+            <div>
+                {
+                    user.map((user) =>{
+                        return(
+                            <div>
+                                <img src="{user.avatar}" alt="" />
+                                <h4>{user.first_name} {user.last_name} </h4>
+                                <h5>{user.email}</h5>
+
+                            <div className="grid grid-rows-3 grid-flow-col gap-4">
+                                <div className="row-span-3">
+                                <button className=" m-5 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">update</button>
+                                </div>
+
+                                <div clas>
+                                <button className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">delet</button>
+                                </div>
+                            </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+           </div>
     )
+
+    function handeleDelet(){
+
+    }
+
+    function handeleCreate(user){
+        
+    }
+
+    function handeleUpdate(user){
+        
+    }
 } 
 export default Users;
