c0c446f93c2bc49a0c2961eaa4cfecd1c4bf26e6
faeze <faezeebold@gmail.com>
2023-06-13 00:55:58 +0330

    httpRequast

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,40 +4,51 @@ import { Component, useState , useEffect } from "react";
 
 const Users = () =>{
 
-  
-    const [users , setusers] = useState([])
 
-    useEffect(() =>{
-        
-    } , [users])
+    function MyComponent (){
 
+        const[users , setUsers] = useState(0);
 
-    async function ComponentDidmount(){
-
-        const response = await axios.get('https://reqres.in/api');
-        ({setusers : response })
+        useEffect(() =>{
+            const response = axios.get('https://reqres.in/api/users');
+            setUsers(response)
+        })
     }
+
+  
+    // const [users , setusers] = useState([])
+
+    // useEffect(() =>{
+
+    //     const response = axios.get('https://reqres.in/api/users');
+    //     ({setusers : response })
+        
+    // } , [users])
+
+
+
 
     
     return(
            <div>
-            <button className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">create</button>
+            <button onClick={handeleCreate} className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">create</button>
             <div>
                 {
-                    users.map((user) =>{
+                     MyComponent.users.map((p) =>{
                         return(
                             <div>
-                                <img src="{users.avatar}" alt="" />
-                                <h4>{users.first_name} {users.last_name} </h4>
-                                <h5>{users.email}</h5>
+                                <img src={ p.avatar} alt="" />
+                                <h4>{p.first_name} {p.last_name} </h4>
+                                <h5>{p.email}</h5>
+                                <h5>{p.id}</h5>
 
                             <div className="grid grid-rows-3 grid-flow-col gap-4">
                                 <div className="row-span-3">
-                                <button className=" m-5 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">update</button>
+                                <button onClick={handeleUpdate} className=" m-5 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">update</button>
                                 </div>
 
-                                <div clas>
-                                <button className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">delet</button>
+                                <div >
+                                <button onClick={handeleDelet} className=" m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">delet</button>
                                 </div>
                             </div>
                             </div>
